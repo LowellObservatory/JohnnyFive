564cc1b1c076d11b533ab10c5c6ab9379ae59a43
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2022-02-24 17:54:54 -0700

    Update install requirements
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1',  # Required
+    version='0.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -91,7 +91,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
@@ -129,9 +129,15 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['python-twitter',
-                      'atlassian-python-api',
-                      'google-api-python-client'],
+    install_requires=['atlassian-python-api', 
+                      'beautifulsoup4',
+                      'google-api-python-client',
+                      'google-auth-httplib2',
+                      'google-auth-oauthlib',
+                      'importlib_resources',
+                      'python-twitter',
+                      'slack_sdk',
+                      'ligmos @ git+https://github.com/LowellObservatory/ligmos'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
